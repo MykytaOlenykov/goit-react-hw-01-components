bb7e5394ab2884c8b0a06d184f484647b92626f3
ForestDr <nikita2001395@gmail.com>
2023-05-22 10:02:38 +0300

    added fix style

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   gap: 40px;
   align-items: center;
-  justify-content: center;
 `;
